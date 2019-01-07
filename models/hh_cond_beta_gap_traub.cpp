85029b1e6531ac8f589c0efc2083f09d070d1d04
dnao <dnao@github.com>
2019-01-07 22:50:06 +1100

    Revert "hh_cond_beta_gap_traub formatting changes"
    This reverts commit 884b0518d25262ec7743153fb27405ec036cbd3f.

--- models/hh_cond_beta_gap_traub.cpp
+++ models/hh_cond_beta_gap_traub.cpp
@@ -150,7 +150,7 @@ hh_cond_beta_gap_traub_dynamics( double time,
   // membrane potential
   f[ S::V_M ] = ( -I_Na - I_K - I_L - I_syn_exc - I_syn_inh + node.B_.I_stim_
                   + I_gap + node.P_.I_e )
-    / node.P_.C_m;
+                  / node.P_.C_m;
 
   // channel dynamics
   const double V = y[ S::V_M ] - node.P_.V_T;
@@ -239,8 +239,8 @@ nest::hh_cond_beta_gap_traub::State_::State_( const State_& s )
   }
 }
 
-nest::hh_cond_beta_gap_traub::State_& nest::hh_cond_beta_gap_traub::
-  State_::operator=( const State_& s )
+nest::hh_cond_beta_gap_traub::State_&
+  nest::hh_cond_beta_gap_traub::State_::operator=( const State_& s )
 {
   assert( this != &s ); // would be bad logical error in program
   for ( size_t i = 0; i < STATE_VEC_SIZE; ++i )
